4ea78db32b2f2aa829902bf8c5c308c48caaa3fc
WIN10OHS20398\hhama <shuichi1105@gmail.com>
2024-10-01 12:15:16 +0900

    laravelプロジェクトの追加

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,70 @@
-# READ ME
+# Laravel研修
 
-Laravelを追加します
+## 用意する物
+1. (windowsの場合。mac,linuxは不要) wslとubuntu
+    * https://www.kkaneko.jp/tools/wsl/wsl2.html
+    * ubuntuは最新のバージョン
+1. PHP 7.4, composer
+   * Windowsの場合
+     * https://windows.php.net/download#php-7.4
+     * https://getcomposer.org/doc/00-intro.md#using-the-installer
+   * Macの場合
+     1. brew install php@7.4
+     1. brew link php@7.4
+     1. brew install composer
+1. Docker
+    * Dockerをインストールする
+    * https://www.docker.com/
+1. Postman
+   * APIクライアントに使用する
+   * https://www.postman.com/downloads/
+   * 利用するために、postmanの無料ユーザー登録をする。
+1. DBクライアント
+   * Windowsの場合
+     * HeidiSQL https://www.heidisql.com/download.php
+   * Macの場合
+     * SequelPro https://sequelpro.com/
+
+## 初期設定
+1. (windowsの場合。mac,linuxは不要) wslのubuntuに入って、作業のフォルダを作って、`git clone`
+    * ubuntu起動するコマンド: `ubuntu` or `wsl`
+    * homeに入る: `cd ~`
+    * 作業フォルダを作る `mkdir laravel`
+    * `cd laravel`
+    * `git clone`
+    * vscodeを起動するコマンド: `code .`
+1. `.env.example` を `.env`にリネームする
+1. `id -u && id -g` コマンドでユーザーIDを確認して、 `.env` ファイルの `DOCKER_UID` と `DOCKER_GID` に同じ値を入れる
+1. `docker-compose up`を実行する
+1. コンテナの中に入る
+    ```
+    docker-compose exec app bash
+    ```
+1. 次のコマンドを実行して依存ライブラリをインストールする
+    ```
+    composer update
+    composer install
+    ```
+1. MySQL上にplayersテーブルを作成する
+    ```
+    php artisan migrate --seed
+    ```
+1. `ctrl + d` を入力して、コンテナからログアウトする。
+
+### 小ワザ
+* 既存のテーブルを作り直して、データも同時に投入する。
+    * `php artisan migrate:refresh --seed`
+
+## Postmanの初期設定
+1. `training-laravel.postman_collection.json` をPostmanにインポートする
+
+## ゲームサーバー起動
+```
+docker-compose up
+```
+
+ゲームサーバーが起動したら、Postmanを起動してsendボタンでゲームサーバーへリクエストを投げてみる。
+
+レスポンスとしてプレイヤー情報が返ってきたらOK。
+
+![img.png](postman.png)
---
+++ app/Http/Controllers/PlayersController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PlayerResource;
+use App\Models\Player;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+class PlayersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new Response(
+            Player::query()->
+            select(['id', 'name'])->
+            get());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+}
